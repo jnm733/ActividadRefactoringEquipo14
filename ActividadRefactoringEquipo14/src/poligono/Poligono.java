2bc4e611b8131fe689fe3988e24ed8e292272c3c
junior@alboradait <junior@alboradait.com>
2016-04-25 11:50:50 +0200

    Extracción de metodo esProductoVectorialPositivo del método concavo en Poligo y eleminacion de variable auxiliares

--- ActividadRefactoringEquipo14/src/poligono/Poligono.java
+++ ActividadRefactoringEquipo14/src/poligono/Poligono.java
@@ -11,51 +11,45 @@ public class Poligono {
 	}
 
 	public boolean concavo(){
-		Vector vector1;
-		Vector vector2;
 
 		//Creamos vectores a partir de los primeros puntos
-		vector1 = new Vector(poligono.get(0), poligono.get(1));
-		vector2 = new Vector(poligono.get(1), poligono.get(2));
-		double prodVectorial = vector.productoVectorial(vector);
-		boolean positivo = prodVectorial >= 0;
+		boolean positivo = esProductoVectorialPositivo(new Vector(poligono.get(0), poligono.get(1)), new Vector(poligono.get(1), poligono.get(2)));
 
 		//Calculamos el signo de los dem�s productos vectoriales
 		for (int i = 1; i < poligono.size()-2; i++) {
-			vector1 = new Vector(poligono.get(i), poligono.get(i+1));
-			vector2 = new Vector(poligono.get(i+1), poligono.get(i+2));
-			prodVectorial = vector.productoVectorial(vec2);
 			//Si tienen distinto signo es c�ncavo
-			if (positivo != (prodVectorial >= 0)) {
+			if (positivo != esProductoVectorialPositivo(new Vector(poligono.get(i), poligono.get(i+1)), new Vector(poligono.get(i+1), poligono.get(i+2)))) {
 				return true;
 			}
 		}
 		//Calculamos el producto vectorial del �ltimo y el primer vector
-		vector1 = new Vector(poligono.get(i+1), poligono.get(0));
-		prodVectorial = vector2.productoVectorial(vec1);
-		if (positivo != (prodVectorial >= 0)) {
+		if (positivo != esProductoVectorialPositivo(new Vector(poligono.get(poligono.size() - 2), poligono.get(poligono.size() - 1)), new Vector(poligono.get(poligono.size() - 1), poligono.get(0)))) {
 			return true;
 		}
 		return false;
+	}
+
+	private boolean esProductoVectorialPositivo(Vector vector1, Vector vector2){
+		return vector1.prodVectorial(vector2) >=0;
 	}
 
 	public Punto centroide(){
 		double X=0;
 		double Y=0;
 		double A=0;
-		int modulo = poligono.size();
+		int moduloulo = poligono.size();
 		for (int k = 0; k < poligono.size(); k++) {
-			A+=poligono.get(k).getPosX()*poligono.get((k+1)%mod).getPosY()-poligono.get((k+1)%mod).getPosX()*poligono.get(k).getPosY();
+			A+=poligono.get(k).getPosX()*poligono.get((k+1)%modulo).getPosY()-poligono.get((k+1)%modulo).getPosX()*poligono.get(k).getPosY();
 		}
 		A=A/2;
 
 		for (int k = 0; k < poligono.size(); k++) {
-			X+=(poligono.get(k).getPosX()+poligono.get((k+1)%mod).getPosX())*(poligono.get(k).getPosX()*poligono.get((k+1)%mod).getPosY()-poligono.get((k+1)%mod).getPosX()*poligono.get(k).getPosY());
+			X+=(poligono.get(k).getPosX()+poligono.get((k+1)%modulo).getPosX())*(poligono.get(k).getPosX()*poligono.get((k+1)%modulo).getPosY()-poligono.get((k+1)%modulo).getPosX()*poligono.get(k).getPosY());
 		}
 		X=X/(6*A);
 
 		for (int k = 0; k < poligono.size(); k++) {
-			Y+=(poligono.get(k).getPosY()+poligono.get((k+1)%mod).getPosY())*(poligono.get(k).getPosX()*poligono.get((k+1)%mod).getPosY()-poligono.get((k+1)%mod).getPosX()*poligono.get(k).getPosY());
+			Y+=(poligono.get(k).getPosY()+poligono.get((k+1)%modulo).getPosY())*(poligono.get(k).getPosX()*poligono.get((k+1)%modulo).getPosY()-poligono.get((k+1)%modulo).getPosX()*poligono.get(k).getPosY());
 		}
 		Y=Y/(6*A);
 		return new Punto((int) X, (int)Y);
